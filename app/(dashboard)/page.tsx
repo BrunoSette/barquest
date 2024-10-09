e162c97f3b93f0954d6f42d26b06c41af0e6980c
Bruno Sette <brunosette@gmail.com>
2024-10-09 10:56:09 -0400

    fix: homepage images

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,10 +2,10 @@ import { Button } from "@/components/ui/button";
 import { ArrowRight, FileQuestion, Database, ChartArea } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import HeroImage from "../public/imagem2.png";
-import AnalyticsImage from "../public/229shots_so.png";
-import QuestionsImage from "../public/questions.png";
-import IphoneMockup from "../public/iphone.png";
+import HeroImage from "../public/lady.webp";
+import AnalyticsImage from "../public/dashboard.webp";
+import QuestionsImage from "../public/questions.webp";
+import IphoneMockup from "../public/iphone.webp";
 import dynamic from "next/dynamic";
 const PricingComponent = dynamic(
   () => import("@/app/(dashboard)/pricing/page"),
@@ -43,8 +43,9 @@ export default function HomePage() {
                 <Image
                   src={HeroImage}
                   alt="Student preparing for Ontario Bar Exam"
-                  width={800}
-                  height={600}
+                  width={1280}
+                  height={800}
+                  quality={100}
                   className="rounded-lg object-contain h-80 w-full"
                 />
               </div>
@@ -121,8 +122,9 @@ export default function HomePage() {
                   className="relative mx-auto rounded-lg object-contain h-full w-full"
                   src={QuestionsImage}
                   alt="Study materials screenshot"
-                  width={600}
-                  height={500}
+                  width={1200}
+                  height={800}
+                  quality={100}
                 />
               </div>
             </div>
@@ -141,8 +143,9 @@ export default function HomePage() {
                   className="relative mx-auto rounded-lg object-contain h-full w-full"
                   src={AnalyticsImage}
                   alt="Performance analytics dashboard"
-                  width={600}
-                  height={500}
+                  width={1187}
+                  height={645}
+                  quality={100}
                 />
               </div>
               <div>
@@ -191,8 +194,9 @@ export default function HomePage() {
                   className="relative mx-auto rounded-lg object-contain h-full w-full"
                   src={IphoneMockup}
                   alt="BarQuest on mobile devices"
-                  width={600}
-                  height={500}
+                  width={956}
+                  height={660}
+                  unoptimized={true}
                 />
               </div>
             </div>
