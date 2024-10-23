76ef6f2e9e6e3157f4c682d7e4b256cb886ac3e3
Bruno Sette <brunosette@gmail.com>
2024-10-22 23:01:59 -0400

    Leave Google Tag Manager

--- app/(dashboard)/pricing/submit-button.tsx
+++ app/(dashboard)/pricing/submit-button.tsx
@@ -3,28 +3,20 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Loader2 } from "lucide-react";
 import { useFormStatus } from "react-dom";
-import { sendGAEvent, sendGTMEvent } from "@next/third-parties/google";
 
 export function SubmitButton() {
   const { pending } = useFormStatus();
 
-  const handleClick = () => {
-    console.log("handleClick gtm e ga begin_checkout")
-    // sendGTMEvent({ event: "event", value: "begin_checkout" });
-    sendGAEvent('event', 'begin_checkout', { value: 'begin_checkout' });
-  }; 
-
   return (
     <Button
-      onClick={handleClick}
-      type="submit" 
+      type="submit"
       disabled={pending}
       className="w-full bg-white hover:bg-orange-400 hover:text-white text-black border border-gray-200 rounded-full flex text-lg items-center justify-center"
     >
       {pending ? (
         <div className="flex items-center">
           <Loader2 className="animate-spin mr-2 h-4 w-4" />
-          Loading...
+          Loading..
         </div>
       ) : (
         <div className="flex items-center">
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata, Viewport } from "next";
 import { Manrope } from "next/font/google";
 import { UserProvider } from "@/lib/auth";
 import { getUser } from "@/lib/db/queries";
-import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -32,8 +32,7 @@ export default function RootLayout({
     >
       <head></head>
       <body className="min-h-[100dvh] bg-gray-50">
-        {/* <GoogleTagManager gtmId="GTM-53LR5KCC" /> */}
-        <GoogleAnalytics  gaId="G-H2CWYESDT8" />
+        <GoogleTagManager gtmId="GTM-53LR5KCC" />
         <SpeedInsights />
         <UserProvider userPromise={userPromise}>{children}</UserProvider>
       </body>
